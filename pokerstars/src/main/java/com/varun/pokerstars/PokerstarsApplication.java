7eff9d03bf82f9c426ef69525a74c33791dd6347
OminousSquash <zcabvsr@ucl.ac.uk>
2025-12-30 14:01:17 +0000

    Added gitignore and setup mysql connection with basic auth

---
+++ pokerstars/src/main/java/com/varun/pokerstars/PokerstarsApplication.java
@@ -0,0 +1,13 @@
+package com.varun.pokerstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerstarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerstarsApplication.class, args);
+	}
+
+}
---
+++ pokerstars/src/test/java/com/varun/pokerstars/PokerstarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.pokerstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerstarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
